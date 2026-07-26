2534fbf225877dc323d0621d1144c8959b26e081
Russell Watson <wats3082@gmail.com>
2026-07-26 11:19:07 -0700

    feat: revamp quiz ui to project standard

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,27 @@
-import "./Game/pages/global.css"
-import type { FC, ReactElement } from "react"
-import QuizCore from "./Game/pages/quizGame/index"
-import ChooseTopic from "./Game/pages/chooseTopic/index"
-import { QuizProvider } from "./Game/contexts/QuizContext"
-import { UniversalProvider } from "./Game/contexts/UniversalContext"
-import {
-  Route,
-  Routes
-} from "react-router-dom";
+import './Game/pages/global.css'
+import type { FC, ReactElement } from 'react'
+import QuizCore from './Game/pages/quizGame/index'
+import ChooseTopic from './Game/pages/chooseTopic/index'
+import { QuizProvider } from './Game/contexts/QuizContext'
+import { UniversalProvider } from './Game/contexts/UniversalContext'
+import { Route, Routes } from 'react-router-dom'
 
-const App:FC = ():ReactElement => {
+const App: FC = (): ReactElement => {
   return (
-    <>
-      <UniversalProvider>
-        <Routes>
-          <Route
-            path="/topic"
-            element={<ChooseTopic />}
-          >
-          </Route>
-            <Route
-              path="/"
-              element={
-                <QuizProvider>
-                  <QuizCore />
-                </QuizProvider>
-              }
-            >
-            </Route>
-        </Routes>
-      </UniversalProvider>
-    </>
-  );
+    <UniversalProvider>
+      <Routes>
+        <Route path="/" element={<ChooseTopic />} />
+        <Route
+          path="/play"
+          element={
+            <QuizProvider>
+              <QuizCore />
+            </QuizProvider>
+          }
+        />
+      </Routes>
+    </UniversalProvider>
+  )
 }
 
-export default App;
+export default App
